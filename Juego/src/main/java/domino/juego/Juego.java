f22d2e821da255be65a46c3772a629700570cc61
Juvee85 <juventino.lopez248547@potros.itson.edu.mx>
2024-10-02 09:03:37 -0700

    commit inicial

---
+++ Juego/src/main/java/domino/juego/Juego.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package domino.juego;
+
+/**
+ *
+ * @author Juventino López García - 00000248547
+ */
+public class Juego {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
