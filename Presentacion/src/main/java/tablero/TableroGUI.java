a0241f43601c9714415da3524c7dfa89848d1c0c
Mostaza5330 <128562434+Mostaza5330@users.noreply.github.com>
2024-10-07 11:34:17 -0700

    .

--- Presentacion/src/main/java/tablero/TableroGUI.java
+++ Presentacion/src/main/java/tablero/TableroGUI.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package tablero;
 
 /**
@@ -10,14 +6,14 @@ package tablero;
  */
 public class TableroGUI extends javax.swing.JFrame {
 
+
+
     /**
      * Creates new form TableroGUI
      */
     public TableroGUI() {
         initComponents();
-      
     }
-
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -28,9 +24,150 @@ public class TableroGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        player2 = new tablero.PanelRound();
+        player1 = new tablero.PanelRound();
+        player3 = new tablero.PanelRound();
+        player4 = new tablero.PanelRound();
+        tableroDomino = new tablero.PanelRound();
+        Fondo = new javax.swing.JPanel();
+        panelRound1 = new tablero.PanelRound();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 0));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        player2.setBackground(new java.awt.Color(255, 255, 102));
+        player2.setRoundBottomLeft(100);
+        player2.setRoundBottomRight(100);
+        player2.setRoundTopLeft(100);
+        player2.setRoundTopRight(100);
+
+        javax.swing.GroupLayout player2Layout = new javax.swing.GroupLayout(player2);
+        player2.setLayout(player2Layout);
+        player2Layout.setHorizontalGroup(
+            player2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        player2Layout.setVerticalGroup(
+            player2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(player2, new org.netbeans.lib.awtextra.AbsoluteConstraints(38, 36, -1, -1));
+
+        player1.setBackground(new java.awt.Color(51, 255, 51));
+        player1.setRoundBottomLeft(100);
+        player1.setRoundBottomRight(100);
+        player1.setRoundTopLeft(100);
+        player1.setRoundTopRight(100);
+
+        javax.swing.GroupLayout player1Layout = new javax.swing.GroupLayout(player1);
+        player1.setLayout(player1Layout);
+        player1Layout.setHorizontalGroup(
+            player1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        player1Layout.setVerticalGroup(
+            player1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(player1, new org.netbeans.lib.awtextra.AbsoluteConstraints(38, 581, -1, -1));
+
+        player3.setBackground(new java.awt.Color(0, 153, 255));
+        player3.setRoundBottomLeft(100);
+        player3.setRoundBottomRight(100);
+        player3.setRoundTopLeft(100);
+        player3.setRoundTopRight(100);
+
+        javax.swing.GroupLayout player3Layout = new javax.swing.GroupLayout(player3);
+        player3.setLayout(player3Layout);
+        player3Layout.setHorizontalGroup(
+            player3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        player3Layout.setVerticalGroup(
+            player3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(player3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1025, 36, -1, -1));
+
+        player4.setBackground(new java.awt.Color(204, 0, 0));
+        player4.setRoundBottomLeft(100);
+        player4.setRoundBottomRight(100);
+        player4.setRoundTopLeft(100);
+        player4.setRoundTopRight(100);
+
+        javax.swing.GroupLayout player4Layout = new javax.swing.GroupLayout(player4);
+        player4.setLayout(player4Layout);
+        player4Layout.setHorizontalGroup(
+            player4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        player4Layout.setVerticalGroup(
+            player4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(player4, new org.netbeans.lib.awtextra.AbsoluteConstraints(1025, 581, -1, -1));
+
+        tableroDomino.setBackground(new java.awt.Color(51, 51, 51));
+        tableroDomino.setRoundBottomLeft(50);
+        tableroDomino.setRoundBottomRight(50);
+        tableroDomino.setRoundTopLeft(50);
+        tableroDomino.setRoundTopRight(50);
+
+        javax.swing.GroupLayout tableroDominoLayout = new javax.swing.GroupLayout(tableroDomino);
+        tableroDomino.setLayout(tableroDominoLayout);
+        tableroDominoLayout.setHorizontalGroup(
+            tableroDominoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 863, Short.MAX_VALUE)
+        );
+        tableroDominoLayout.setVerticalGroup(
+            tableroDominoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 518, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(tableroDomino, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 107, -1, -1));
+
+        Fondo.setBackground(new java.awt.Color(28, 28, 28));
+
+        panelRound1.setBackground(new java.awt.Color(102, 102, 102));
+        panelRound1.setRoundBottomLeft(50);
+        panelRound1.setRoundBottomRight(50);
+        panelRound1.setRoundTopLeft(50);
+        panelRound1.setRoundTopRight(50);
+
+        javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
+        panelRound1.setLayout(panelRound1Layout);
+        panelRound1Layout.setHorizontalGroup(
+            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 272, Short.MAX_VALUE)
+        );
+        panelRound1Layout.setVerticalGroup(
+            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 77, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout FondoLayout = new javax.swing.GroupLayout(Fondo);
+        Fondo.setLayout(FondoLayout);
+        FondoLayout.setHorizontalGroup(
+            FondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(FondoLayout.createSequentialGroup()
+                .addGap(146, 146, 146)
+                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(742, Short.MAX_VALUE))
+        );
+        FondoLayout.setVerticalGroup(
+            FondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, FondoLayout.createSequentialGroup()
+                .addContainerGap(639, Short.MAX_VALUE)
+                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(14, 14, 14))
+        );
+
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1160, 730));
 
         pack();
         setLocationRelativeTo(null);
@@ -67,11 +204,21 @@ public class TableroGUI extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TableroGUI().setVisible(true);
+                TableroGUI gui = new TableroGUI();
+                gui.setVisible(true);
             }
         });
     }
 
+  
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel Fondo;
+    private tablero.PanelRound panelRound1;
+    private tablero.PanelRound player1;
+    private tablero.PanelRound player2;
+    private tablero.PanelRound player3;
+    private tablero.PanelRound player4;
+    private tablero.PanelRound tableroDomino;
     // End of variables declaration//GEN-END:variables
 }
